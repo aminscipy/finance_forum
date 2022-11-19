0d870267626bc88b93cca3927c06140a8cd8bf94
amin <thisisaamin@gmail.com>
2022-11-19 14:03:41 +0530

    onesignal push notification working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:finance_forum/controllers/auth_controller.dart';
 import 'package:finance_forum/controllers/add_stock_controller.dart';
 import 'package:finance_forum/controllers/post_controller.dart';
@@ -8,6 +10,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'controllers/profile_controller.dart';
 import 'package:firebase_app_installations/firebase_app_installations.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,6 +29,14 @@ void main() async {
   ], builder: (context, child) => const MyApp()));
   print(await FirebaseInstallations.instance.getId());
   print(await FirebaseMessaging.instance.getToken());
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.setAppId('d2170fac-5b53-4027-b364-bb0fd026fa49');
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
 }
 
 class MyApp extends StatelessWidget {
